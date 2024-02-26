ad48f756206dbb68c2311c754bdd4cadebc80581
Eric O'Brien <cireneirbo@gmail.com>
2024-02-26 15:57:01 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,28 @@
+const periodContribution = 800;
+const periodsPerYear = 24;
+const interestRatePerYear = 1.11;
+const yearsToInvest = 10;
+
+
+function calculateInterestPerYear() {
+
+  let sum = 0;
+  
+  // add to sum for each year of investment
+  for(let y = 0; y < yearsToInvest; y++){
+
+    // add contributions for an entire year
+    for(let i = 0; i < periodsPerYear; i++) {
+      sum += periodContribution;
+    }
+
+    // multiply interest per year
+    sum *= interestRatePerYear;
+  }
+
+  console.log(`If you invest $${periodContribution}, at an interval of ${periodsPerYear} times per year, for ${yearsToInvest} ${yearsToInvest > 1 ? "years" : "year"}, at a return rate of ${Math.round((interestRatePerYear * 100) - 100)}%, you will amass: $${Math.round(sum)}`);
+
+  return Math.round(sum);
+}
+
+calculateInterestPerYear();
